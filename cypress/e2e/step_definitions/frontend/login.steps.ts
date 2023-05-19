a1473d077c3ff74617b578b9a526e9d20a409d9b
qui.ngo <phuqui220991@gmail.com>
2023-05-19 19:10:15 +0700

    Fix login.steps issues

--- cypress/e2e/step_definitions/frontend/login.steps.ts
+++ cypress/e2e/step_definitions/frontend/login.steps.ts
@@ -6,18 +6,18 @@ Given('I am on the login page', () => {
 })
 
 When(
-  'I enter valid credentials (username: {string}, password: {string})',
-  (Username: string, Password: string) => {
-    loginPage.enterEmail(Username)
-    loginPage.enterPassword(Password)
+  'I enter valid credentials \\(username: {string}, password: {string}\\)',
+  (username: string, password: string) => {
+    loginPage.enterEmail(username)
+    loginPage.enterPassword(password)
   }
 )
 
 When(
-  'I enter invalid credentials (username: {string}, password: {string})',
-  (Username: string, Password: string) => {
-    loginPage.enterEmail(Username)
-    loginPage.enterPassword(Password)
+  'I enter invalid credentials \\(username: {string}, password: {string}\\)',
+  (username: string, password: string) => {
+    loginPage.enterEmail(username)
+    loginPage.enterPassword(password)
   }
 )
 
