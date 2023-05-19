a0c8ac0afbb33d0258383dccb983ff2240786069
qui.ngo <phuqui220991@gmail.com>
2023-05-19 19:00:41 +0700

    Add sample test scripts for Login Function

--- cypress/e2e/step_definitions/frontend/login.steps.ts
+++ cypress/e2e/step_definitions/frontend/login.steps.ts
@@ -5,28 +5,32 @@ Given('I am on the login page', () => {
   loginPage.load()
 })
 
-When('I enter valid credentials', () => {
-  loginPage.enterEmail('standard_user')
-  loginPage.enterPassword('secret_sauce')
-})
+When(
+  'I enter valid credentials (username: {string}, password: {string})',
+  (Username: string, Password: string) => {
+    loginPage.enterEmail(Username)
+    loginPage.enterPassword(Password)
+  }
+)
 
-When('I enter invalid credentials', () => {
-  loginPage.enterEmail('locked_out_user')
-  loginPage.enterPassword('secret_sauce')
-})
+When(
+  'I enter invalid credentials (username: {string}, password: {string})',
+  (Username: string, Password: string) => {
+    loginPage.enterEmail(Username)
+    loginPage.enterPassword(Password)
+  }
+)
 
 When('I click the login button', () => {
   loginPage.clickLoginButton()
 })
 
-Then('I should be redirected to the home page', () => {
-  cy.url().should('contain', 'https://www.saucedemo.com/inventory.html')
+Then('I should see an error message is {string}', (message: string) => {
+  loginPage.getErrorMessage().should('contain.text', message)
 })
 
-Then('I should see an error message', () => {
-  loginPage
-    .getErrorMessage()
-    .should('contain.text', 'Sorry, this user has been locked out.')
+Then('I should be redirected to the home page', () => {
+  cy.url().should('contain', 'https://www.saucedemo.com/inventory.html')
 })
 
 Then('I should not be redirected to the home page', () => {
