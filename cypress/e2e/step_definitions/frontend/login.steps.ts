35b1fde18aef3187449adb0d047e0e26d2d9e482
qui.ngo <phuqui220991@gmail.com>
2023-05-19 11:25:44 +0700

    Add eslint & prettierr

--- cypress/e2e/step_definitions/frontend/login.steps.ts
+++ cypress/e2e/step_definitions/frontend/login.steps.ts
@@ -1,32 +1,34 @@
-import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
-import { loginPage } from '@pages/loginPage';
+import { When, Then, Given } from '@badeball/cypress-cucumber-preprocessor'
+import { loginPage } from '@pages/loginPage'
 
-Given('I am on the login page', ()=> {
-    loginPage.load();
-})
-    
-When('I enter valid credentials', ()=> {
-    loginPage.enterEmail("standard_user");
-    loginPage.enterPassword("secret_sauce");
-})
-   
-When('I enter invalid credentials', ()=> {
-    loginPage.enterEmail("locked_out_user");
-    loginPage.enterPassword("secret_sauce");
+Given('I am on the login page', () => {
+  loginPage.load()
 })
 
-When('I click the login button', ()=> {
-    loginPage.clickLoginButton();
+When('I enter valid credentials', () => {
+  loginPage.enterEmail('standard_user')
+  loginPage.enterPassword('secret_sauce')
 })
 
-Then('I should be redirected to the home page', ()=> {
-    cy.url().should('contain', 'https://www.saucedemo.com/inventory.html');
+When('I enter invalid credentials', () => {
+  loginPage.enterEmail('locked_out_user')
+  loginPage.enterPassword('secret_sauce')
 })
 
-Then('I should see an error message', ()=> { 
-    loginPage.getErrorMessage().should('contain.text', 'Sorry, this user has been locked out.');
+When('I click the login button', () => {
+  loginPage.clickLoginButton()
 })
 
-Then('I should not be redirected to the home page', ()=> {
-    cy.url().should('contain', 'https://www.saucedemo.com/');
+Then('I should be redirected to the home page', () => {
+  cy.url().should('contain', 'https://www.saucedemo.com/inventory.html')
+})
+
+Then('I should see an error message', () => {
+  loginPage
+    .getErrorMessage()
+    .should('contain.text', 'Sorry, this user has been locked out.')
+})
+
+Then('I should not be redirected to the home page', () => {
+  cy.url().should('contain', 'https://www.saucedemo.com/')
 })
--- cypress/pages/loginPage.ts
+++ cypress/pages/loginPage.ts
@@ -1,23 +1,25 @@
 class LoginPage {
-	load() {
-		cy.visit('/');
-	}
+  load() {
+    cy.visit('/')
+  }
 
-	enterEmail(email: string) {
-		cy.get('#user-name').clear().type(email);
-	}
-	
-	enterPassword(password: string) {
-		cy.get('#password').clear().type(password);
-	}
-	
-	clickLoginButton() {
-		cy.get('#login-button').click();
-	}
-	
-	getErrorMessage() {
-		return cy.get('h3[data-test="error"]');
-	}
+  enterEmail(email: string) {
+    cy.get('#user-name').clear()
+    cy.get('#user-name').type(email)
+  }
+
+  enterPassword(password: string) {
+    cy.get('#password').clear()
+    cy.get('#password').type(password)
+  }
+
+  clickLoginButton() {
+    cy.get('#login-button').click()
+  }
+
+  getErrorMessage() {
+    return cy.get('h3[data-test="error"]')
+  }
 }
 
-export const loginPage = new LoginPage();
+export const loginPage = new LoginPage()
